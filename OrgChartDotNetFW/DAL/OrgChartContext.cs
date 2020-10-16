02bbde00092bd002ea8a82f7ffc19542b0b24d37
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-10-16 16:13:47 +0300

    First commit

---
+++ OrgChartDotNetFW/DAL/OrgChartContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using OrgChartDotNetFW.Models;
+
+namespace OrgChartDotNetFW.DAL
+{
+    public class OrgChartContext : DbContext
+    {
+        public OrgChartContext() : base("OrgChartContext")
+        {
+
+        }
+
+        public DbSet<Node> Nodes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
---
+++ OrgChartDotNetFW/Models/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OrgChartDotNetFW.Models
+{
+    public class Node
+    {
+        public int Id { get; set; }
+        public int? Pid { get; set; }
+        public int? Stpid { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Img { get; set; } 
+
+        [NotMapped]
+        public string[] Tags
+        {
+            get
+            {
+                return InternalTags.Split(',');
+            }
+            set
+            {
+                InternalTags = String.Join(",", value);
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public string InternalTags { get; set; }
+
+   
+
+    }
+}
