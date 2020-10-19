2f9da4aad3e9ae9b0b02cf3ff15fdc836df03fbc
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-10-19 12:16:08 +0300

    Plamen's fix

---
+++ OrgChartDotNetFW/Controllers/ChartController.cs
@@ -0,0 +1,28 @@
+﻿using OrgChartDotNetFW.DAL;
+using OrgChartDotNetFW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrgChartDotNetFW.Controllers
+{
+    public class ChartController : Controller
+    {
+        private OrgChartContext db = new OrgChartContext();
+
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Load()
+        {
+            var nodes = db.Nodes.ToList();
+            return Json(nodes, JsonRequestBehavior.AllowGet);
+        }    
+    }
+}
--- OrgChartDotNetFW/Models/Node.cs
+++ OrgChartDotNetFW/Models/Node.cs
@@ -3,36 +3,56 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Policy;
 using System.Web;
 
 namespace OrgChartDotNetFW.Models
 {
     public class Node
     {
+        //public Node()
+        //{
+        //    this.Tags = new List<Tag>();
+        //}
+
         public string Id { get; set; }
         public string Pid { get; set; }
         public string Stpid { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
-        public string Img { get; set; } 
+        public string Img { get; set; }
+
+        //   public virtual IList<Tag> Tags { get; set; }
+
 
         [NotMapped]
         public string[] Tags
         {
             get
             {
-                return InternalTags.Split(',');
+                if (string.IsNullOrEmpty(InternalTags))
+                {
+                    return new string[0];
+                }
+                else 
+                {
+                    return InternalTags.Split(',');
+                }
             }
             set
             {
-                InternalTags = string.Join(",", value);
+                if (value == null)
+                {
+                    InternalTags = "";
+                }
+                else
+                {
+                    InternalTags = string.Join(",", value);
+                }
             }
         }
 
         [EditorBrowsable(EditorBrowsableState.Never)]
         public string InternalTags { get; set; }
-
-   
-
     }
 }
